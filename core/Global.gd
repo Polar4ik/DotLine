f1da287b3aaa843187e29be5319b1131f40256bf
Polar4ik <atossnikitoss@gmail.com>
2024-08-10 00:26:59 +0400

    added switch to change window mode

--- core/Global.gd
+++ core/Global.gd
@@ -4,3 +4,13 @@ extends Node
 signal game_over
 
 var game_end := false
+
+var full_screene = false
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("fullscren_togle"):
+		full_screene = !full_screene
+		if full_screene:
+			get_window().mode = Window.MODE_FULLSCREEN
+		elif full_screene == false:
+			get_window().mode = Window.MODE_WINDOWED
